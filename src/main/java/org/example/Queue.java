0583c95b349cdafe0b51212daebb6efe9be7d156
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2023-11-22 13:51:23 +0530

    Implement Queue

---
+++ src/main/java/org/example/Queue.java
@@ -0,0 +1,81 @@
+package org.example;
+
+/**
+ * Description:
+ * Author: ruu
+ * Created: 2023-11-22 12.40
+ */
+public class Queue {
+    private int[] queue;
+    private int front; //head
+    private int rear; //tail
+
+    public Queue(int size) {
+        queue = new int[size];
+        front = -1;
+        rear = -1;
+    }
+
+    public void enQueue(int data) {
+        if (isFull()) {
+            System.out.println("Queue is full");
+        } else {
+            if (front == -1) {
+                front = 0;
+            }
+            rear++;
+            queue[rear] = data;
+            System.out.println(data + " added to the queue");
+        }
+    }
+
+    public boolean isFull(){
+        return rear == queue.length - 1;
+    }
+
+    public int deQueue() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty");
+            return -1;
+        } else {
+            return queue[front++];
+        }
+    }
+
+    public boolean isEmpty() {
+        return front == -1;
+    }
+
+    public void printQueue() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty");
+        } else {
+            System.out.println("Queue: ");
+            for (int i = front; i <= rear; i++) {
+                System.out.print(queue[i]+"  ");
+            }
+            System.out.println();
+        }
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty");
+            return -1;
+        } else {
+            return queue[front];
+        }
+    }
+
+    public int size() {
+        return rear - front + 1;
+    }
+
+    public void growQueue() {
+        int[] newQueue = new int[queue.length * 2];
+        System.arraycopy(queue, 0, newQueue, 0, queue.length);
+        queue = newQueue;
+        System.out.println("Queue size is doubled, new size is : "+queue.length);
+    }
+
+}
---
+++ src/main/java/org/example/QueueMain.java
@@ -0,0 +1,53 @@
+package org.example;
+
+/**
+ * Description:
+ * Author: ruu
+ * Created: 2023-11-22 12.40
+ */
+public class QueueMain {
+    public static void main(String[] args) {
+        Queue queue = new Queue(5);
+
+        queue.enQueue(3);
+        queue.enQueue(4);
+        queue.enQueue(5);
+        queue.enQueue(6);
+        queue.enQueue(6);
+
+        System.out.println();
+
+        if(queue.isFull()) {
+            System.out.println("Queue is full");
+        } else {
+            System.out.println("Queue is not full");
+        }
+
+        int size = queue.size();
+        System.out.println("Size: " + size);
+
+        int data = queue.deQueue();
+        System.out.println("Dequeued: " + data);
+
+        if(queue.isFull()) {
+            System.out.println("Queue is full");
+        } else {
+            System.out.println("Queue is not full");
+        }
+
+        size = queue.size();
+        System.out.println("Size: " + size);
+
+        queue.printQueue();
+
+        int peek = queue.peek();
+        if (peek != -1) {
+            System.out.println("Peek: " + peek);
+        }
+
+        queue.growQueue();
+
+
+
+    }
+}
